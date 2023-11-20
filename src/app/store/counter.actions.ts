0bba3611226b3190a16d64cbd4242de512892aeb
Abdeslam Ezzaghi <60900845+AbdeslamEzzaghi@users.noreply.github.com>
2023-11-20 23:58:32 +0100

    added actions in two ways

---
+++ src/app/store/counter.actions.ts
@@ -0,0 +1,20 @@
+import { createAction, props,Action } from "@ngrx/store";
+
+
+
+
+
+
+
+/*export const increment = createAction(
+    '[Counter] Increment',
+    props<{value:number}>()
+    )
+
+*/
+export class IncrementAction implements Action{
+    readonly type = '[Counter] Increment';
+    constructor(public value : number){
+
+    }
+}
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,13 +1,17 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
+//import { increment } from "./counter.actions";
 
 const initialState = 0;
 
-export const counterReducer = createReducer(
-    initialState
+/*export const counterReducer = createReducer(
+    initialState ,
+    on(increment,(state,action) => state+action.value)
 ); 
 
-/*
-export function counterReducer(state = initialState){
+*/
+export function counterReducer(state = initialState,action:any){
+    if(action.type === "[Counter] Increment"){
+        return state + action.value;
+    }
     return state;
 }
-*/ 
