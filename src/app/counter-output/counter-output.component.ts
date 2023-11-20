90d3926ae7e8e46ca3467eca9f6db811fa10c376
Abdeslam Ezzaghi <60900845+AbdeslamEzzaghi@users.noreply.github.com>
2023-11-20 15:35:00 +0100

    added the reducer

---
+++ src/app/counter-output/counter-output.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+
+import { CounterService } from '../counter.service';
+import { Store } from '@ngrx/store';
+
+@Component({
+  selector: 'app-counter-output',
+  templateUrl: './counter-output.component.html',
+  styleUrls: ['./counter-output.component.css'],
+})
+export class CounterOutputComponent  {
+  counter$ : Observable<number>;
+
+  constructor(private store : Store<{counter:number}>) {
+    this.counter$ = store.select('counter')
+  }
+
+}
